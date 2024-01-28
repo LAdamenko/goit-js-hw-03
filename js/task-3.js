932123a382ecc62afb9dcf9dd392ab009e0f589c
LAdamenko <148965340+LAdamenko@users.noreply.github.com>
2024-01-28 18:50:10 +0200

    Done

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,15 @@
-function getElementWidth(content, padding, border) {
-    content = Number.parseFloat(content);
-    padding = Number.parseFloat(padding);
-    border = Number.parseFloat(border);
-    content = Number(content);
-    padding = Number(padding);
-    border = Number(border);
-    const boxSizing = content + padding * 2 + border * 2;
-    return boxSizing;
+function filterArray(numbers, value) {
+    const maxNumbers = [];
+    for (const number of numbers) {
+        if (number > value) {
+            maxNumbers.push(number);
+        }
+    }
+    return maxNumbers;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
